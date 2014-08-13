ee3ddb34c82bce959ea9f9fb2591a970733a175a
Thomas Tuegel <ttuegel@gmail.com>
2014-08-13 09:17:45 -0500

    Simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -1,20 +1,5 @@
-{ pkgs ? (import <nixpkgs> {}), haskellPackages ? pkgs.haskellPackages_ghc783
-, hsDevTools ? pkgs.hsDevTools }:
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages_ghc783
+}:
 
-let inherit (haskellPackages)
-      cabal exceptions filepath resourcet tasty tastyHunit transformers;
-in
-cabal.mkDerivation (self: {
-  pname = "temporary-resourcet";
-  version = "1.2.0.3";
-  src = ./.;
-  buildDepends = [ exceptions filepath resourcet transformers ];
-  testDepends = [ tasty tastyHunit ];
-  buildTools = hsDevTools haskellPackages;
-  meta = {
-    homepage = "http://www.github.com/ttuegel/temporary-resourcet";
-    description = "Portable temporary files and directories with automatic deletion";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+haskellPackages.callPackage ./default.nix {}
